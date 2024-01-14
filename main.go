1688ace63c60cd8d662057cd5e92f46619fbf34c
fastrapier <daniil.evgenyevich703@gmail.com>
2024-01-14 17:57:14 +0300

    Парсинг конфигуратора реализован

--- main.go
+++ main.go
@@ -1,109 +1,97 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gocolly/colly/v2"
 	"log"
+	"os"
+	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type ConfiguratorProduct struct {
-	Id    int
-	Title string
-	Price int
+	ID       int32
+	Title    string
+	Price    int32
+	PriceEur float32
 }
+
+type Configuration struct {
+	Name                 string
+	ConfiguratorProducts []ConfiguratorProduct
+}
+
 type Configurator struct {
-	Title                string
-	Link                 string
-	Price                int
-	ConfiguratorProducts map[string][]ConfiguratorProduct
+	Title          string
+	Price          int32
+	PriceEur       int32
+	Link           string
+	Configurations []Configuration
 }
+
+type configuratorsMap map[string]*Configurator
+
+var euro = 105
 
 func main() {
-
 	c := colly.NewCollector()
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
-	configuratorProducts := c.Clone()
+	c.OnError(func(response *colly.Response, err error) {
+		log.Fatalf("Link: %s -> %s", response.Request.URL.String(), err)
+	})
 
-	var configurators = collectConfigurators(*c)
+	c.OnRequest(func(request *colly.Request) {
+		log.Printf("Visiting: %s", request.URL.String())
+	})
 
-	for i, configurator := range configurators {
-		fmt.Printf("Index: %d | %+v\n", i, configurator)
+	configurators := make(configuratorsMap)
+
+	configurators.fill(*c)
+
+	jsonData, err := json.MarshalIndent(configurators, "", "\t")
+
+	if err != nil {
+		log.Fatalf("Err:%s", err)
 	}
 
-	if len(configurators) == 0 {
-		log.Fatal("Empty configurators!")
-		return
-	}
+	now := time.Now().Format(time.DateTime)
 
-	configurators = collectConfiguratorProducts(*configuratorProducts, configurators)
-}
-
-func collectConfiguratorProducts(collector colly.Collector, configurators []Configurator) []Configurator {
-
-	fmt.Println("Start collecting configurator products!")
-
-	collector.OnHTML("table.product-specifications tbody tr", func(element *colly.HTMLElement) {
-		specification := strings.TrimSpace(element.ChildText("td:eq(0)"))
-		// Todo: Нужно продумать логику извлечения данных из столбцов
-		fmt.Printf("Start parsing specification %s\n", specification)
-
-		var configuratorProducts []ConfiguratorProduct
-
-		element.ForEach("td:eq(1) select option", func(i int, element *colly.HTMLElement) {
-
-			id, err := strconv.Atoi(element.Attr("value"))
+	if _, err := os.Stat("./tmp"); err != nil {
+		if os.IsNotExist(err) {
+			err := os.Mkdir("tmp", 0777)
 
 			if err != nil {
-				fmt.Printf("Link:%s | err:%s\n", element.Request.URL.String(), err.Error())
+				panic(err)
 			}
-
-			title := element.Text
-
-			price, err := strconv.Atoi(element.Attr("data-price"))
-
-			if err != nil {
-				fmt.Printf("Link:%s | err:%s\n", element.Request.URL.String(), err.Error())
-			}
-
-			configuratorProduct := ConfiguratorProduct{
-				Id:    id,
-				Title: title,
-				Price: price,
-			}
-			fmt.Printf("configurator product: %+v\n", configuratorProduct)
-			configuratorProducts = append(configuratorProducts, configuratorProduct)
-		})
-
-		for _, configurator := range configurators {
-			configurator.ConfiguratorProducts[specification] = configuratorProducts
+		} else {
+			panic(err)
 		}
-	})
-
-	collector.OnError(func(response *colly.Response, err error) {
-		fmt.Printf("Link:%s -> err:%s", response.Request.URL.String(), err.Error())
-	})
-
-	collector.OnRequest(func(request *colly.Request) {
-		fmt.Printf("Visiting:%s\n", request.URL.String())
-	})
-
-	for i, configurator := range configurators {
-		fmt.Printf("Start parsing #%d | %+v\n", i, configurator)
-		collector.Visit(configurator.Link)
 	}
 
-	return configurators
+	jsonFile, err := os.Create("./tmp/configurators-" + now + ".json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer jsonFile.Close()
+
+	jsonFile.Write(jsonData)
+	jsonFile.Close()
+
+	//productsCollector := configuratorCollector.Clone()
+	//
+	//productsCollector.Visit("https://sale-server.ru")
 }
 
-func collectConfigurators(collector colly.Collector) []Configurator {
-	var configurators []Configurator
+func (configurators configuratorsMap) fill(c colly.Collector) {
 
-	fmt.Println("Start collecting configurators!")
+	c.OnHTML("div.products figure.product-item", func(element *colly.HTMLElement) {
 
-	collector.OnHTML("div.products figure.product-item", func(element *colly.HTMLElement) {
 		title := strings.TrimSpace(element.ChildAttr("figcaption.info h3 a", "title"))
 
 		link := element.ChildAttr("figcaption.info h3 a", "href")
@@ -119,27 +107,71 @@ func collectConfigurators(collector colly.Collector) []Configurator {
 		price, err := strconv.Atoi(priceText)
 
 		if err != nil {
-			fmt.Printf("Price not found - %s\n", title)
+			fmt.Printf("Price not found - %s %s\n", title, link)
 		}
 
-		configurator := Configurator{
-			Title: title,
-			Link:  link,
-			Price: price,
+		configurators[link] = &Configurator{
+			Title:          title,
+			Price:          int32(price),
+			PriceEur:       int32(price / euro),
+			Link:           link,
+			Configurations: make([]Configuration, 0),
 		}
 
-		configurators = append(configurators, configurator)
+		c.Visit(link)
 	})
 
-	collector.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visiting", request.URL.String())
+	c.OnHTML("table.product-specifications > tbody > tr", func(element *colly.HTMLElement) {
+		html, err := element.DOM.Find("td:nth-child(1)").Html()
+
+		if err != nil {
+			log.Fatalf("%s", err)
+			return
+		}
+
+		configuration := Configuration{
+			Name:                 strings.TrimSpace(html),
+			ConfiguratorProducts: make([]ConfiguratorProduct, 0),
+		}
+
+		element.ForEach("td:nth-of-type(2) select option", func(i int, element *colly.HTMLElement) {
+
+			if element.Attr("value") == "0" || strings.Contains(element.Text, "Нет в наличии") {
+				return
+			}
+
+			id, err := strconv.Atoi(element.Attr("value"))
+
+			if err != nil {
+				log.Printf("ID - Link:%s | err:%s\n", element.Request.URL.String(), err)
+			}
+
+			title := strings.TrimSpace(element.Text)
+
+			priceRegex, err := regexp.Compile("-\\s+\\d+.*₽")
+
+			if err != nil {
+				log.Fatalf("%s", err)
+			}
+
+			title = strings.TrimSpace(priceRegex.ReplaceAllString(title, ""))
+
+			price, err := strconv.Atoi(element.Attr("data-price"))
+
+			if err != nil {
+				log.Printf("PRICE - Link:%s | err:%s\n", element.Request.URL.String(), err)
+			}
+
+			configuratorProduct := ConfiguratorProduct{
+				ID:       int32(id),
+				Title:    title,
+				Price:    int32(price),
+				PriceEur: float32(price / euro),
+			}
+
+			configuration.ConfiguratorProducts = append(configuration.ConfiguratorProducts, configuratorProduct)
+		})
+		configurators[element.Request.URL.String()].Configurations = append(configurators[element.Request.URL.String()].Configurations, configuration)
 	})
-
-	collector.OnError(func(response *colly.Response, err error) {
-		log.Fatalf("%s", err.Error())
-	})
-
-	collector.Visit("https://sale-server.ru/konfigurator/products")
-
-	return configurators
+	c.Visit("https://sale-server.ru/konfigurator/products")
 }
