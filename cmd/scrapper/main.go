0e25e778702df140f605654d230e6900ea7b30af
fastrapier <daniil.evgenyevich703@gmail.com>
2024-01-16 22:31:15 +0300

    add config.ini

--- cmd/scrapper/main.go
+++ cmd/scrapper/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"gopkg.in/ini.v1"
+	"os"
 	"scrapper/internal/parser"
 	"scrapper/internal/utils"
 )
 
 func main() {
-	euro := 105.12
+	cfg, err := ini.Load("./config/config.ini")
+
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+	euro, err := cfg.Section("").Key("euro").Float64()
+
+	if err != nil {
+		fmt.Printf("Fail to read euro key: %v", err)
+		os.Exit(1)
+	}
 
 	configuratorsMap := parser.ParseConfigurator(euro)
+
 	utils.WriteToJson(configuratorsMap)
 }
